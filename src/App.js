e01518cf034201005b66dbcbcd80268b32971817
Asj-code <andreasilvinaj@gmail.com>
2021-11-18 15:34:51 -0300

    blockbuster movies project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Movie from "./Movie";
+import MoviesJson from "./movies.json";
+import Pages from "./Pages";
+import PageWrapper from "./PageWrapper";
+import {useState} from 'react';
+import {useEffect} from 'react';
 
 function App() {
+
+  const [actualPage, setActualPage] = useState(1)
+  // const [allMovies, setAllMovies] = useState([]);
+  const moviesForPage = 5;
+
+  let movies = MoviesJson;
+
+  // useEffect(() => {
+  // searchMovies();
+  // }, []);
+
+  // const searchMovies = async () => {
+  //   let url = 'https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/lucasmoy-dev/Curso-de-React/main/Proyecto%202%20-%20Web%20de%20Peliculas/Proyecto%20Terminado/src/peliculas.json'
+    
+    
+  //   let response = await fetch(url, {
+  //     "method": 'GET',
+  //     "mode": 'no-cors',
+  //     "headers": {
+  //       "Accept": 'application/json',
+  //       "Content-type": 'application/json'
+  //     }
+  //   });
+  //   let json = await response.json();
+  //   alert(json)
+  // setAllMovies(json)
+  // }
+
+
+
+
+
+
+  const uploadMovies = () => {
+    movies = movies.slice((actualPage - 1) * moviesForPage, actualPage * moviesForPage);
+  }
+
+  const getTotalPages = () => {
+    let quantityTotalMovies = MoviesJson.length;
+    return Math.ceil(quantityTotalMovies / moviesForPage)
+  }
+
+  uploadMovies();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+
+      {/* <button onClick={searchMovies()}></button> */}
+
+      {movies.map(movie =>
+      <Movie
+      img={movie.img}
+      title={movie.title}
+      stars={movie.stars}
+      director={movie.director}
+      actors={movie.actors}
+      date={movie.date}
+      runTime={movie.runTime}
+      >{movie.description}
+      </Movie>
+    )}
+
+    <Pages page={actualPage} total={moviesForPage} onChange={page=> setActualPage(page)}/>
+
+    </PageWrapper>
   );
 }
 
